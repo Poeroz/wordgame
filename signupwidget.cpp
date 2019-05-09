1a8f4d5c28565dfa5b81a275db882570ead9b62d
poeroz <poeroz1204@gmail.com>
2019-05-09 08:40:00 +0800

    add password confirm.

--- signupwidget.cpp
+++ signupwidget.cpp
@@ -14,9 +14,10 @@ signUpWidget::signUpWidget(QWidget *parent) :
     ui->setupUi(this);
 
     /* 使用正则表达式约束 */
-    ui->usrLineEdit->setValidator(new QRegExpValidator(QRegExp("^[A-Za-z0-9]{1,20}$"), this));
-    ui->nameLineEdit->setValidator(new QRegExpValidator(QRegExp("^[A-Za-z0-9\u4e00-\u9fa5]{1,20}$"), this));
-    ui->pwdLineEdit->setValidator(new QRegExpValidator(QRegExp("^.{1,20}$"), this));
+    ui->usrLineEdit->setValidator(new QRegExpValidator(QRegExp("^[A-Za-z0-9]{1,10}$"), this));
+    ui->nameLineEdit->setValidator(new QRegExpValidator(QRegExp("^[A-Za-z0-9\u4e00-\u9fa5]{1,10}$"), this));
+    ui->pwdLineEdit->setValidator(new QRegExpValidator(QRegExp("^.{1,10}$"), this));
+    ui->pwdLineEdit_2->setValidator(new QRegExpValidator(QRegExp("^.{1,10}$"), this));
 }
 
 signUpWidget::~signUpWidget() {
@@ -27,6 +28,7 @@ void signUpWidget::on_okBtn_clicked() {
     QString usr = ui->usrLineEdit->text();
     QString name = ui->nameLineEdit->text();
     QString pwd = ui->pwdLineEdit->text();
+    QString pwd2 = ui->pwdLineEdit_2->text();
     QRadioButton* btn = qobject_cast<QRadioButton*>(ui->buttonGroup->checkedButton());
     if (btn == nullptr) {
         QMessageBox::warning(this, tr("提示"), tr("请选择你的角色"), QMessageBox::Ok);
@@ -40,16 +42,20 @@ void signUpWidget::on_okBtn_clicked() {
         else {
             role = QUESTIONER;
         }
-        if (usr.length() < 6) {
-            QMessageBox::warning(this, tr("提示"), tr("用户名长度必须在 6 到 20 之间"), QMessageBox::Ok);
+        if (usr.length() < 3) {
+            QMessageBox::warning(this, tr("提示"), tr("用户名长度必须在 3 到 10 之间"), QMessageBox::Ok);
             ui->usrLineEdit->setFocus();
         }
-        else if (name.length() < 6) {
-            QMessageBox::warning(this, tr("提示"), tr("昵称长度必须在 6 到 20 之间"), QMessageBox::Ok);
+        else if (name.length() < 3) {
+            QMessageBox::warning(this, tr("提示"), tr("昵称长度必须在 3 到 10 之间"), QMessageBox::Ok);
             ui->nameLineEdit->setFocus();
         }
-        else if (pwd.length() < 6) {
-            QMessageBox::warning(this, tr("提示"), tr("密码长度必须在 6 到 20 之间"), QMessageBox::Ok);
+        else if (pwd.length() < 3) {
+            QMessageBox::warning(this, tr("提示"), tr("密码长度必须在 3 到 10 之间"), QMessageBox::Ok);
+            ui->pwdLineEdit->setFocus();
+        }
+        else if (pwd != pwd2) {
+            QMessageBox::warning(this, tr("提示"), tr("两次密码输入不一致"), QMessageBox::Ok);
             ui->pwdLineEdit->setFocus();
         }
         else {
@@ -75,6 +81,7 @@ void signUpWidget::init() {
     ui->usrLineEdit->clear();
     ui->pwdLineEdit->clear();
     ui->nameLineEdit->clear();
+    ui->pwdLineEdit_2->clear();
     ui->usrLineEdit->setFocus();
     ui->buttonGroup->setExclusive(false);
     ui->playerBtn->setChecked(false);
